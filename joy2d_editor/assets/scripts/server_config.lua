4cb0ccc4d3a961359fbb9bd653d380bff90f4b41
code210 <228407354@qq.com>
2026-03-08 12:02:36 +0800

    test: joy2d_editor测试资源文件

---
+++ joy2d_editor/assets/scripts/server_config.lua
@@ -0,0 +1,12 @@
+local config = {}
+
+--config.ip = "172.24.9.215"
+config.ip = "192.168.1.13"
+config.port = 30000
+config.start = "scripts/server2.lua"
+config.loader = "lualib/loader.lua"
+config.luapath = "lualib/?.lua;scripts/?.lua"
+config.luacpath = "luaclib/?.dll;luaclib/?.so;"
+config.cpath = ""
+
+return config
